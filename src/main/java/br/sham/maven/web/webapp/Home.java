a6d64310798c19ecf6edc71a27fc17c99fe6e25d
skatesham <sham.vincius@gmail.com>
2018-12-12 20:16:52 -0200

    imagem

--- src/main/java/br/sham/maven/web/webapp/Home.java
+++ src/main/java/br/sham/maven/web/webapp/Home.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import br.com.alura.maven.App;
+//import br.com.alura.maven.App;
 import br.com.caelum.stella.tinytype.CPF;
 
 //@WebServlet(urlPatterns={"/"})
@@ -19,7 +19,7 @@ public class Home extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println(new CPF("22222222222").isValido());
 		System.out.println(new CPF("05325855182").isValido());
-		App.main(new String[0]);
+		//App.main(new String[0]);
 		req.getRequestDispatcher("/templates/index.jsp").forward(req, resp);
 	}
 	
